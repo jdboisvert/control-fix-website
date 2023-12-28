857535c5e9011115ae29833b66ce70d87553a028
jdboisvert <info.jeffreyboisvert@gmail.com>
2023-12-28 18:11:25 -0500

    feat: Updated data for controller 3.

--- controller-fix/src/data/ControllerData.js
+++ controller-fix/src/data/ControllerData.js
@@ -1,27 +1,28 @@
+// TODO for now this is just a placeholder for the controller data later to be in a proper datastore
 export const ControllerData = {
     3: {
         title: 'Clear Blue Nintendo 64 Controller',
         description: 'Replaced the top gear and greased.',
         imgUrl: '/images/controller_3/controller_3.001.png',
         joystickPositionsBefore: [
-            { x: 3, y: 78 },
-            { x: 63, y: 68 },
-            { x: 82, y: -1 },
-            { x: 70, y: -68 },
-            { x: 2, y: -80 },
-            { x: -63, y: -72 },
-            { x: -75, y: -8 },
-            { x: -64, y: 65 },
+            { x: -2, y: 79 }, // Up
+            { x: 63, y: 64 }, // Up-Right
+            { x: 72, y: -3 }, // Right
+            { x: 57, y: -74 }, // Down-Right
+            { x: -2, y: -88 }, // Down
+            { x: -72, y: -79 }, // Down-Left
+            { x: -89, y: -4 }, // Left
+            { x: -69, y: 64 }, // Up-Left
         ],
         joystickPositionsAfter: [
-            { x: 1, y: 82 },
-            { x: 62, y: 71 },
-            { x: 78, y: 1 },
-            { x: 62, y: -68 },
-            { x: 0, y: -81 },
-            { x: -60, y: -70 },
-            { x: -75, y: 0 },
-            { x: -58, y: 69 },
+            { x: 0, y: 88 }, // Up
+            { x: 70, y: 76 }, // Up-Right
+            { x: 89, y: 0 }, // Right
+            { x: 70, y: -71 }, // Down-Right
+            { x: 0, y: -83 }, // Down
+            { x: -66, y: -71 }, // Down-Left
+            { x: -81, y: 0 }, // Left
+            { x: -65, y: 72 }, // Up-Left
         ],
     },
 };
